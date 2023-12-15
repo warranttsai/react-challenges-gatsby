ffade42f9472c4190a94ac6b6763aacdf48b81ca
warrant <warrant1997@gmail.com>
2023-12-15 19:09:27 +1100

    feat: add images to about me

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Layout from "../../components/layout";
+import Instagram from "../../../static/instagram-icon.png";
+import Github from "../../../static/github-icon.png";
+import LinkedIn from "../../../static/linkedin-icon.png";
+import ProfilePic from "../../../static/profile-pic.png";
 
 export default function AboueMe({ data, location }) {
   return (
@@ -8,19 +12,50 @@ export default function AboueMe({ data, location }) {
         className="content-grid"
         style={{
           backgroundColor: "#dda15e",
-          paddingBlock: 200,
         }}
       >
         <div
-          className="d-flex"
+          className="d-flex flex-wrap"
           style={{
-            backgroundColor: "#fefae0",
+            backgroundColor: "#d3d3d3",
             borderRadius: 10,
-            padding: "10px 20px",
+            padding: "20px 50px",
+            marginBlock: 100,
+            gap: 10,
           }}
         >
-          <div className="flex-2">1</div>
-          <div className="flex-1">2</div>
+          <div className="flex-2 d-flex flex-column" style={{ gap: 40 }}>
+            <h1 className="text-left" style={{ fontSize: 50 }}>
+              WARRANT TSAI
+            </h1>
+            <p className="text-left" style={{ fontSize: 14 }}>
+              Results-driven web developer with one and a half years of
+              proficiency in React JS/TS development. Recently conferred a
+              master's degree in Information Technology (2022), demonstrating a
+              commitment to continuous learning. Proven expertise in backend
+              development utilizing Lambda serverless framework, Node.JS,
+              Sails.JS, and Python Flask. Known for an insatiable appetite for
+              acquiring new knowledge, paired with a reputation for
+              responsibility, organizational prowess, and a willingness to
+              tackle new challenges head-on. An open-minded individual receptive
+              to diverse perspectives and suggestions.
+            </p>
+            <div
+              className="d-flex justify-content-center aling-items-center"
+              style={{ gap: 50 }}
+            >
+              <img src={Instagram} style={{ width: 50 }} alt="" />
+              <img src={LinkedIn} style={{ width: 50 }} alt="" />
+              <img src={Github} style={{ width: 50 }} alt="" />
+            </div>
+          </div>
+          <div className="flex-1 d-flex justify-content-center align-items-center">
+            <img
+              src={ProfilePic}
+              style={{ borderRadius: 20, width: "100%", minWidth: 20 }}
+              alt=""
+            />
+          </div>
         </div>
       </section>
     </Layout>
